5973023288d6219c9d0c5c89b701d36e963daa2a
Johann <contact@johannroehl.de>
2017-05-19 10:03:06 +0200

    P2P (#3)
    * ci update
    
    * added config, templates, comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
-    'require-jsdoc': 'warn',
     'comma-dangle': 'off',
-    'prefer-template': 'warn',
-    strict: 'off',
-    'import/no-unresolved': 'warn'
+    strict: 'off'
   }
 };
--- handler.js
+++ handler.js
@@ -1,29 +1,43 @@
 'use strict';
 
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 
-const getCredentials = (domain) => {
-  let pass;
-  switch (domain) {
-    case 'pavillon-am-ufer.de':
-      pass = 'RHtHgNzoWsC7*>Dhgm4JmyMD';
-      break;
-    case 'heidpartner.com':
-      pass = 'nmFPq8HpzezN9y=)wzXaMzbg';
-      break;
-    default:
-      // no-reply@johannroehl.de
-      pass = 'KtnGKUBd3RLgPyHgRoyfikjE';
-  }
-  return {
-    user: `no-reply@mg.${domain || 'johannroehl.de'}`,
-    pass
-  };
+const { domains, self, mailgun } = require('./config.js');
+
+/**
+ * Replace all occurences of placeholders in template specified as key/value pairs
+ *
+ * @param {object} replaceOptions object with keys to be replaced in template
+ * @param {string} templatePath path to template
+ */
+const processTemplate = (replaceOptions, templatePath) => {
+  const template = fs.readFileSync(templatePath);
+  return Object.keys(replaceOptions)
+    .reduce(
+      (tmp, key) => tmp.replace(new RegExp(`@@${key.toUpperCase()}`, 'g'),
+        replaceOptions[key]), template);
 };
 
-module.exports.sendmail = (event = {}, context, callback) => {
-  const self = 'roehl.johann@gmail.com';
+/**
+ * get smtp configuration according to domain
+ *
+ * @param {string} senderDomain
+ */
+const getSMTPconfig = (senderDomain) => {
+  const sender =
+    domains.filter(({ domain }) => domain === senderDomain)[0] ||
+    domains.filter(domain => domain.default)[0];
+  delete sender.domain;
+  delete sender.default;
 
+  return Object.assign({}, mailgun, sender);
+};
+
+/**
+ * main handler
+ */
+module.exports.sendmail = (event = {}, context, callback) => {
   const {
     mail = '',
       name = '',
@@ -49,58 +63,34 @@ module.exports.sendmail = (event = {}, context, callback) => {
     });
     callback(null, response);
   } else {
-    const auth = getCredentials(domain);
-    const smtpTransport = nodemailer.createTransport(Object.assign({}, {
-      port: 587,
-      host: 'smtp.mailgun.org',
-      tls: {
-        rejectUnauthorized: false
-      }
-    }, {
-      auth
-    }));
+    const config = getSMTPconfig(domain);
+    const smtpTransport = nodemailer.createTransport(config);
+
+    const replaceOptions = {
+      mail,
+      name,
+      message,
+      surname,
+      phone,
+      bymail: bymail ? `mail: ${mail}` : '',
+      byphone: byphone ? `phone: ${phone}` : '',
+      recom: recom ? `RECOMMENDATION ${recom}` : recom
+    };
 
     // setup e-mail data with unicode symbols
     const mailOptions = {
-      from: auth.user.replace('mg.', ''), // sender address
+      from: config.user.replace('mg.', ''), // sender address
       to: recipient, // list of receivers
       subject: `NEW MAIL from ${domain} contact form - ${mail}`, // Subject line
       // replyTo: sender,
       // plaintext body
-      text: `NEW MAIL from ${mail}\n
-            SENDER ${name} ${surname} | ${mail} ${phone}\n
-            MESSAGE ${message}\n
-            ${recom ? `RECOMMENDATION ${recom}<br>\n` : ''}
-            CONTACTBY ${bymail ? `mail: ${mail}` : ''} ${byphone ? `phone: ${phone}` : ''}`,
+      text: processTemplate(replaceOptions, './templates/mail.template.txt'),
       // html body
-      html: `<h2 style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">NEW MAIL</h2>
-            <table>
-              <tr>
-                <td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">
-                  FROM
-                </td>
-                <td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">
-                  ${name} ${surname} | ${mail} ${phone}
-                </td>
-              </tr>
-              <tr>
-                <td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">
-                  MESSAGE
-                </td>
-                <td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">
-                  ${message}
-                </td>
-              </tr>
-              ${recom ? `<tr><td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">RECOMMENDATION</td><td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">${recom}</td></tr>` : ''}
-              <tr>
-                <td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">
-                  CONTACT
-                </td>
-                <td style="font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; font-weight: 100">
-                  ${bymail ? `mail: ${mail}` : ''} ${byphone ? `phone: ${phone}` : ''}
-                </td>
-              </tr>
-            </table>`
+      html: processTemplate(
+        Object.assign({}, replaceOptions, {
+          recom: recom ? processTemplate(replaceOptions,
+            './templates/recom.template.html') : recom
+        }), './templates/mail.template.html')
     };
 
     // send mail with defined transport object
@@ -110,11 +100,13 @@ module.exports.sendmail = (event = {}, context, callback) => {
       response.statusCode = err === null ? 200 : err.responseCode;
 
       response.body = JSON.stringify({
-        message: err === null ? `${Date()} | Mail sent to ${recipient}` : err.message
+        message: err === null ?
+          `${Date()} | Mail sent to ${recipient}` : err.message
       });
       callback(null, response);
     });
 
+    // log to cloudwatch
     console.log(
       `${Date()} | ${domain} | message ${mail} ${name} ${surname} ${message}`
     );

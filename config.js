c064052b98430a2731c88d5e68972c38794c3869
jroehl <contact@johannroehl.de>
2017-06-29 07:26:30 +0200

    tests

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 const self = 'roehl.johann@gmail.com';
 
 const domains = [{
-  domain: 'pavillon-am-ufer.de',
-  user: 'no-reply@mg.pavillon-am-ufer.de',
-  pass: 'RHtHgNzoWsC7*>Dhgm4JmyMD'
-},
-{
   domain: 'heidpartner.com',
   user: 'no-reply@mg.heidpartner.com',
   pass: 'nmFPq8HpzezN9y=)wzXaMzbg'
@@ -13,10 +8,9 @@ const domains = [{
 {
   default: true,
   domain: 'johannroehl.de',
-  user: 'no-reply@mg.johannroehl.de',
+  user: 'no-reply@johannroehl.de',
   pass: 'KtnGKUBd3RLgPyHgRoyfikjE'
-}
-];
+}];
 
 const mailgun = {
   port: 587,

dd7f2f008ca787699e49c924fc437360b52d5add
jroehl <mail@johannroehl.de>
2018-05-06 15:06:00 +0200

    new email templates and cleanup

--- handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { callbackHandler } = require('./lib/helpers/misc');
-const sendmail = require('./lib/sendmail');
-
-module.exports.sendmail = (event, context, callback) => {
-  const response = callbackHandler(callback);
-  return sendmail(event, context, response);
-};

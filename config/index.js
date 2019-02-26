29e51b6130247d6c85640c6673b0fc19e4c7b291
jroehl <mail@johannroehl.de>
2019-02-26 23:44:09 +0100

    fix tests

--- config/index.js
+++ config/index.js
@@ -40,7 +40,7 @@ module.exports = {
     user: 'no-reply',
     endpoint: 'info',
     validationFields: {
-      ignore: ['receiver'],
+      ignore: ['receiver', 'domain'],
       invalid: ['honeypot'],
       required: [
         'mail',
